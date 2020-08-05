7e064dec61d99c89752516f83a99ea1b01bcea3d
Wesley Terwey <jellisky@me.com>
2020-08-05 15:54:05 -0500

    First Commit

---
+++ Pages/Scheduling.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SynopticForecastWebsite2.Pages
+{
+    public class SchedulingModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
